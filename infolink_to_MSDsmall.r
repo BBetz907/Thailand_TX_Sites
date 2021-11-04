55415a81a046ae7c948e101b54c861de4ae4b6dd
BBetz907 <BBETZ@USAID.GOV>
2021-11-03 20:57:03 -0700

    Data export ready for merge

--- infolink_to_MSDsmall.r
+++ infolink_to_MSDsmall.r
@@ -12,15 +12,14 @@ table(thailand_sites_infolink$dataname)
 
 thailand_custom <- thailand_sites_infolink %>%
   clean_names() %>% 
-  tidyr::pivot_longer(cols = contains("_2"), names_to = "month_year", values_to = "Values") %>%
-  dplyr::filter(str_detect(dataname,"VERIFY"),
-                !is.na(Values)) %>%
-  dplyr::mutate('Country Name' = orgunitlevel2,
-                Psnu = orgunitlevel3,
-                Sitename = orgunitlevel5,
-                Indicator = if_else(str_detect(dataname, "TX_PVLS_VERIFY"), "TX_PVLS_VERIFY", dataname),
-                'Funding Agency' = "USAID",
-                'Standardized Disaggregate' = if_else(dataname == "TX_PVLS_VERIFY (D)", "Total Denominator", "Total Numerator"),
+  tidyr::pivot_longer(cols = contains("_2"), names_to = "month_year", values_to = "values") %>%
+  dplyr::filter(str_detect(dataname,"VERIFY"), !is.na(values)) %>%
+  dplyr::mutate(country_name = orgunitlevel2,
+                PSNU = orgunitlevel3,
+                sitename = orgunitlevel5,
+                indicator = if_else(str_detect(dataname, "TX_PVLS_VERIFY"), "TX_PVLS_VERIFY", dataname),
+                funding_agency = "USAID",
+                standardized_disaggregate = if_else(dataname == "TX_PVLS_VERIFY (D)", "Total Denominator", "Total Numerator"),
                 month = stringr::str_extract(month_year, "^.*(?=_20)"), #STR PRECEDED BY "_20"
                 q = recode(month,
                            "december" = "Q1",
@@ -29,8 +28,8 @@ thailand_custom <- thailand_sites_infolink %>%
                            "september" = "Q4"),
                 fiscal_year = if_else(month=="december", as.double(stringr::str_extract(month_year, "(?<=_20).*"))+1, as.double(stringr::str_extract(month_year, "(?<=_20).*"))),
                 FY = str_c("FY",fiscal_year,sep = ""),
-                Quarter = str_c(FY,q, sep = " ")) %>%
-  select('Country Name', 'Funding Agency', Psnu, Sitename, 'Standardized Disaggregate', Indicator, Values, FY, Quarter) %>%
+                quarter = str_c(FY,q, sep = " ")) %>%
+  select(country_name, funding_agency, PSNU, sitename, standardized_disaggregate, indicator, values, FY, quarter) %>%
   glimpse()
 
 write.csv(thailand_custom, "dataout/Thailand FY20-21 Custom Indicators.csv", na = "", row.names = FALSE)
